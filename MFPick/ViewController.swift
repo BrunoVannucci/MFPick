04fc7ec7792de8539cd58c1ffd825ff6731ac112
Bruno Vannucci <brunovannucci@Brunos-MacBook-Air.local>
2015-11-29 14:51:17 -0200

    Initial Commit

---
+++ MFPick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MFPick
+//
+//  Created by Bruno Vannucci on 11/29/15.
+//  Copyright (c) 2015 BrunoVannucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
